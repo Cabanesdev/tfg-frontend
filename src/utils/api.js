dfcdaa370342809d7a646fcadc3d4bf657828142
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-05-26 11:29:38 +0200

    chore(): to production

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ import { getSession } from './localstorage';
 
 export default class Api {
   constructor() {
-    this.api_url = 'http://localhost:3000';
+    this.api_url = 'http://api.cabanesdev.tk';
     const token = getSession();
 
     if (token) {
